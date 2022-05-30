af28aad20e522f8d144790cb04a21c3cfd6bce68
mosesokemwa <715660-mosesokemwa@users.noreply.gitlab.com>
2022-05-30 22:24:13 +0300

    (house keeping) cleaning code files
    Signed-off-by: mosesokemwa <715660-mosesokemwa@users.noreply.gitlab.com>

--- Dockerfile
+++ Dockerfile
@@ -22,5 +22,5 @@ USER upload
 
 EXPOSE 5000
 
-CMD gunicorn --worker-class gevent --workers 8 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5 --log-level info
+CMD gunicorn --worker-class gevent --workers 3 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5 --log-level info
 # ENTRYPOINT ["./boot.sh"]
--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source .venv/bin/activate
-exec gunicorn --worker-class gevent --workers 8 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5 --log-level info
+source venv/bin/activate
+exec gunicorn --worker-class gevent --workers 3 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5 --log-level info
